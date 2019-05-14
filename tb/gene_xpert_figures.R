fc8347abc4318426b90d78926d205b338a50d394
Caitlin O'Brien-Carelli <ccarelli@ihme.washington.edu>
2019-05-14 11:39:28 -0700

    Update gene_xpert_figures.R

--- tb/gene_xpert_figures.R
+++ tb/gene_xpert_figures.R
@@ -16,14 +16,14 @@ library(plyr)
 library(RColorBrewer)
 library(raster)
 library(maptools)
+library(LaCroixColoR)
 # --------------------
 
 #------------------------------------------------
 # to code in the same code, we need to set file pathways for each user
-# change the username to your username to code
-# for example: user = 'copio'
+# this command automatically detects your username!
 
-user = 'ccarelli'
+user = Sys.info()[['user']]
 
 # -----------------------------------------------
 # input and output directories
@@ -126,6 +126,7 @@ turq = '#80cdc1'
 ratios = c('#bd0026', '#3182bd', '#74c476', '#feb24c')
 sex_colors = c('#bd0026', '#3182bd', '#74c476', '#8856a7')
 lav = '#bebada'
+croix = lacroix_palette("Pamplemousse", n = 50, type = "continuous")
 
 #-------------------------------------------------------------
 # CREATE MAPS AND GRAPHS 
@@ -133,7 +134,7 @@ lav = '#bebada'
 # --------------------------------------------------------------
 # exploratory graphs
 
-# pdf(paste0(outDir, 'tb_sample_figures.pdf'), height=9, width=12)
+pdf(paste0(outDir, 'tb_sample_figures.pdf'), height=9, width=12)
 
 #------------------------------
 # reporting by genexpert sites
@@ -163,7 +164,7 @@ ggplot(rep, aes(x=region, y=rate, color=all, size=sites, label=sites)) +
   theme_bw() +
   ylim(80, NA) +
   scale_color_manual(values=true_false) +
-  labs(title="Percent of GeneXpert reporting (%), April 1 - 7, 2019", y="Percent reporting (%)", x="Region", color="100% reported", 
+  labs(title="Percent of GeneXpert sites reporting (%), April 1 - 7, 2019", y="Percent reporting (%)", x="Region", color="100% reported", 
        size="Number of sites", subtitle='Value label = total number of GeneXpert sites') +
   theme(text = element_text(size=14), axis.text.x=element_text(size=12, angle=90)) 
 
@@ -191,15 +192,33 @@ ggplot(rep_level, aes(x=level, y=sites, fill='GeneXpert Sites', label=sites)) +
 # calculate the counts and percentages of all samples
 kids = dt[ ,.(total_samples = sum(total_samples), children = sum(children_under_14)), by=region]
 kids[ ,ratio:=round(100*children/total_samples, 1)]
-kids = melt(kids, id.vars='region')
+
+# shape the data long
+kids_long = melt(kids, id.vars='region')
 
 # label the variables 
-kids$variable = factor(kids$variable, c('total_samples', 'children', 'ratio'),
+kids_long$variable = factor(kids_long$variable, c('total_samples', 'children', 'ratio'),
                         c('Total samples', 'Samples submitted by children < 14',
                         'Percentage of samples submitted by children < 14 (%)'))
 
+
+# colors for the stacked bar graphs of samples subkitted by children 
+kid_colors = c('Total samples'='#998ec3', 'Samples submitted by children < 14'='#f1a340')
+percent = round(100*kids[ ,sum(children)/sum(total_samples)], 1)
+
 # bar graph of reporting compared to total sites 
-ggplot(kids[variable!='Total samples'], aes(x=region, y=value, fill=variable, label=value)) +
+ggplot(kids, aes(x=region, y=total_samples, fill='Total samples', label=total_samples)) +
+  geom_bar(stat="identity") +
+  geom_bar(aes(y=children, fill='Samples submitted by children < 14'), stat="identity") + 
+  geom_text(size = 4, position=position_stack(vjust = 1.06)) +
+  scale_fill_manual(name='', values=kid_colors) + theme_minimal() +
+  labs(x='Region', y='Samples', 
+       subtitle = paste0(percent, '% of all samples'), 
+       title="Samples submitted by children under 14") +
+  theme(text = element_text(size=18), axis.text.x=element_text(size=12, angle=90))
+
+# bar graph of reporting compared to total sites 
+ggplot(kids_long[variable!='Total samples'], aes(x=region, y=value, fill=variable, label=value)) +
   geom_bar(stat="identity") +
   facet_wrap(~variable, scales='free_y') +
   geom_text(size = 4, position=position_stack(vjust = 0.5)) +
@@ -208,7 +227,6 @@ ggplot(kids[variable!='Total samples'], aes(x=region, y=value, fill=variable, la
   labs(x='Region', y=' ', title="Samples submitted by children under 14, count and percentage") +
   theme(text = element_text(size=18), axis.text.x=element_text(size=12, angle=90)) +
   guides(fill=FALSE)
-
 
 #------------------------------
 # count of machines by region and implementer
@@ -287,7 +305,7 @@ part[ ,error_rate:=round(100*total_errors/total_samples, 1)]
 part_sub = part[ ,.(total_samples, tb_positive) , by=impl_partner]
 part_sub = melt(part_sub, id.vars='impl_partner')
 
-
+# tb positive samples by implementing partner
 count_labels2 = part[ ,.(total_samples = sum(total_samples), tb_positive=sum(tb_positive))]
 part_sub$variable = factor(part_sub$variable, c('total_samples', 'tb_positive'),
                            c(paste0('Total samples (n = ', count_labels$total_samples, ')'),
@@ -348,56 +366,85 @@ ggplot(compare[variable!='TB positive'], aes(x=region, y=value, fill=variable)) 
   theme(text = element_text(size=18), axis.text.x=element_text(size=12, angle=90)) 
 
 #------------------------------------------------------------------------------------
-# maps 
+# MAPS OF PROPORTIONS AND COUNTS 
+
+# calculate sample size for labels
+samples = dt[ ,sum(total_samples)]
+retreats = dt[ ,sum(retreatments)]
+pos = dt[ ,sum(tb_positive)]
+childs = dt[ ,sum(children_under_14)]
 
 # total samples
 ggplot(coord, aes(x=long, y=lat, group=group, fill=total_samples)) + 
   geom_polygon() + 
   geom_path(size=0.01, color="#636363") + 
-  scale_fill_gradientn(colors=brewer.pal(9, 'Greens')) + 
+  scale_fill_gradientn(colors=brewer.pal(9, 'Blues')) + 
   theme_void() + 
-  labs(title="Total samples by district", subtitle="April 1 - 7, 2019",
+  labs(title="Total samples by district", subtitle=paste0("n = ", samples,  "; April 1 - 7, 2019"),
        fill="Total samples") +
-  theme(plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
+  theme(text=element_text(size=18), plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
         plot.caption=element_text(vjust=6)) + coord_fixed()
 
 # retreatments 
 ggplot(coord, aes(x=long, y=lat, group=group, fill=retreatments)) + 
   geom_polygon() + 
   geom_path(size=0.01, color="#636363") + 
-  scale_fill_gradientn(colors=brewer.pal(9, 'Purples')) + 
+  scale_fill_gradientn(colors=brewer.pal(9, 'Greens')) + 
   theme_void() + 
-  labs(title="Total retreatments by district", subtitle="April 1 - 7, 2019",
-       fill="Samples from retreatments") +
-  theme(plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
+  labs(title="Total retreatments by district", 
+       subtitle=paste0("n = ", retreats, "; April 1 - 7, 2019"),
+       fill="Retreatments") +
+  theme(text=element_text(size=18), plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
+        plot.caption=element_text(vjust=6)) + coord_fixed()
+
+# children under 14 
+ggplot(coord, aes(x=long, y=lat, group=group, fill=children_under_14)) + 
+  geom_polygon() + 
+  geom_path(size=0.01, color="#636363") + 
+  scale_fill_gradientn(colors=brewer.pal(9, 'YlOrRd')) + 
+  theme_void() + 
+  labs(title="Children under 14 submitting samples by district", subtitle="April 1 - 7, 2019",
+       fill=" ") +
+  theme(text=element_text(size=18), plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
         plot.caption=element_text(vjust=6)) + coord_fixed()
 
 # tb positive samples 
 ggplot(coord, aes(x=long, y=lat, group=group, fill=tb_positive)) + 
   geom_polygon() + 
   geom_path(size=0.01, color="#636363") + 
-  scale_fill_gradientn(colors=brewer.pal(9, 'Blues')) + 
+  scale_fill_gradientn(colors=brewer.pal(9, 'Purples')) + 
   theme_void() + 
-  labs(title="TB positive samples by district", subtitle="April 1 - 7, 2019",
+  labs(title="TB positive samples by district", subtitle=paste0("n = ", pos, "; April 1 - 7, 2019"),
        fill="TB positive (count)") +
-  theme(plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
+  theme(text=element_text(size=18), plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
         plot.caption=element_text(vjust=6)) + coord_fixed()
 
 # rifampicin resistant 
 ggplot(coord, aes(x=long, y=lat, group=group, fill=rif_resist)) + 
   geom_polygon() + 
   geom_path(size=0.01, color="#636363") + 
-  scale_fill_gradientn(colors=brewer.pal(9, 'Reds'), breaks = c(0, 1)) + 
+  scale_fill_gradientn(colors=c('#fcfbfd', '#a50f15'), breaks = c(0, 1)) + 
   theme_void() + 
   labs(title="Rifampicin resistant samples by district", subtitle="Two districts with one RR sample each; April 1 - 7, 2019",
        fill=" ") +
-  theme(plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
+  theme(text=element_text(size=18), plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
         plot.caption=element_text(vjust=6)) + coord_fixed()
 
+# rifampicin indeterminate
+ggplot(coord, aes(x=long, y=lat, group=group, fill=rif_indet)) + 
+  geom_polygon() + 
+  geom_path(size=0.01, color="#636363") + 
+  scale_fill_gradientn(colors=brewer.pal(9, 'GnBu'), breaks = c(0, 3, 6, 9)) + 
+  theme_void() + 
+  labs(title="Rifampicin indeterminate samples by district", subtitle="Two districts with one RR sample each; April 1 - 7, 2019",
+       fill=" ") +
+  theme(text=element_text(size=18), plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
+        plot.caption=element_text(vjust=6)) + coord_fixed()
 
 # -------------------------------------
-# percentages of TB positive 
+# rifampicin resistant and indeterminate
 
+# positive for tb, rif, or indeterminate by region 
 pos = reg[ ,.(tb_positive, rif_resist, rif_indet), by=region]
 
 # create totals for labels 
@@ -411,7 +458,6 @@ pos = melt(pos, id.vars='region')
 pos[variable=='ind_ratio', variable:=paste0('Rifampicin indeterminate (n = ', labels$rif_indet, ')')]
 pos[variable=='res_ratio', variable:=paste0('Rifampicin resistant (n = ', labels$rif_resist, ')')]
 
-
 # rifampicin indeterminate, rifampicin resistant
 ggplot(pos, aes(x=region, y=value, fill=variable)) +
   geom_bar(position ="dodge", stat="identity") +
@@ -422,26 +468,67 @@ ggplot(pos, aes(x=region, y=value, fill=variable)) +
        fill='', subtitle=paste0('n = ', labels$tb_positive)) +
   theme(text = element_text(size=18), axis.text.x=element_text(size=12, angle=90)) 
 
-
 # -------------------------------------
 # maps of percentages 
 
+# percentage tb positive and rifampicin resistant 
 idVars = c('id', 'lat', 'long', 'order', 'hole', 'piece', 'group')
-new = coord[ ,.(percent_pos, percent_res), by=idVars]
-new = melt(new, id.vars=idVars)
+perc = coord[ ,.(percent_pos, percent_res), by=idVars]
+perc = melt(perc, id.vars=idVars)
 
-new$variable = factor(new$variable, c('percent_pos', 'percent_res'),
+perc$variable = factor(perc$variable, c('percent_pos', 'percent_res'),
                       c('Percentage of tests that were TB positive',
                         'Percentage of TB positive tests that were RR'))
 
-# rates by district
-ggplot(new, aes(x=long, y=lat, group=group, fill=value)) + 
+ggplot(perc, aes(x=long, y=lat, group=group, fill=value)) + 
   geom_polygon() + 
   geom_path(size=0.01, color="#636363") + 
-  facet_wrap(~variable, scales='free_y') +
-  scale_fill_gradientn(colors=brewer.pal(11, 'PuBuGn')) + 
+  facet_wrap(~variable) +
+  scale_fill_gradientn(colors=brewer.pal(9, 'PuBuGn')) + 
   theme_void() + 
-  labs(fill="%") +   theme(text = element_text(size=18))
+  labs(fill="%") +
+  theme(text=element_text(size=18), plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
+        plot.caption=element_text(vjust=6)) + coord_fixed()
+
+# percent tb positive
+ggplot(coord, aes(x=long, y=lat, group=group, fill=percent_pos)) + 
+  geom_polygon() + 
+  geom_path(size=0.01, color="#636363") + 
+  scale_fill_gradientn(colors=brewer.pal(9, 'Blues')) + 
+  theme_void() + 
+  labs(title = "Percentage of samples that were positive for TB" , fill="%") +
+  theme(text=element_text(size=18), plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
+        plot.caption=element_text(vjust=6)) + coord_fixed()
+
+# percent RR
+ggplot(coord, aes(x=long, y=lat, group=group, fill=percent_res)) + 
+  geom_polygon() + 
+  geom_path(size=0.01, color="#636363") + 
+  scale_fill_gradientn(colors=brewer.pal(9, 'Reds')) + 
+  theme_void() + 
+  labs(title = "Percentage of samples that were Rifampicin resistant" , fill="%") +
+  theme(text=element_text(size=18), plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
+        plot.caption=element_text(vjust=6)) + coord_fixed()
+
+# percent indeterminate for rifampicin
+ggplot(coord, aes(x=long, y=lat, group=group, fill=percent_indet)) + 
+  geom_polygon() + 
+  geom_path(size=0.01, color="#636363") + 
+  scale_fill_gradientn(colors=brewer.pal(9, 'YlOrBr')) + 
+  theme_void() + 
+  labs(title = "Percentage of samples that were Rifampicin indeterminate" , fill="%") +
+  theme(text=element_text(size=18), plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
+        plot.caption=element_text(vjust=6)) + coord_fixed()
+
+# error rateggplot(coord, aes(x=long, y=lat, group=group, fill=error_rate)) + 
+  geom_polygon() + 
+  geom_path(size=0.01, color="#636363") + 
+  scale_fill_gradientn(colors=croix) + 
+  theme_void() + 
+  labs(title = "Error rate", subtitle='Percenate of samples that resulted in an error', 
+       fill="Percent errors (%)") +
+  theme(text=element_text(size=18), plot.title=element_text(vjust=-4), plot.subtitle=element_text(vjust=-4), 
+        plot.caption=element_text(vjust=6)) + coord_fixed()
 
 # ----------------------------------------------------------
 # implementing partners  
@@ -455,7 +542,8 @@ ggplot(part, aes(x=reorder(impl_partner, -total_samples), y=total_samples, fill=
   labs(x='Implementing Partner', y='Total Samples', title='Total samples by implementing partner',
        subtitle=paste0('Total samples: ', total_part), 
        fill=" ") +
-  theme(text = element_text(size=18), axis.text.x=element_text(size=12, angle=90))
+  theme(text = element_text(size=18), axis.text.x=element_text(size=12, angle=90)) +
+  guides(fill=F)
 
 # ---------------------------
 #  percent TB positive
@@ -467,7 +555,7 @@ ggplot(part, aes(x=reorder(impl_partner, -percent_pos), y=percent_pos, label=per
   scale_fill_manual(values=lav) + 
   theme_minimal() +
   labs(x='Implementing partner', y='Percent positive (%)', 
-       title="Percentage of total samples that were positive for TB") +
+       title="Percentage of total samples that were positive for TB by implementing partner") +
   theme(text = element_text(size=18), axis.text.x=element_text(size=12, angle=90)) +
   guides(fill=FALSE)
 
@@ -486,7 +574,7 @@ ggplot(compare_part, aes(x=impl_partner, y=value, fill=variable)) +
   theme_minimal() +
   scale_fill_manual(values=ratios) +
   labs(x='Implementing Partner', y='Percent (%)', 
-       title="Percentage of total samples", 
+       title="Percentage of total samples by implementing partner", 
        fill='') +
   theme(text = element_text(size=18), axis.text.x=element_text(size=12, angle=90)) 
 # ---------------------------
