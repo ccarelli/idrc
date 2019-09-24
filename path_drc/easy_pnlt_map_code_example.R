c12657ffb5ca52c34d3ed5a5d11535f4741f64b3
Caitlin O'Brien-Carelli <ccarelli@ihme.washington.edu>
2019-09-24 15:56:04 -0700

    Add previous versions of mapping code

--- path_drc/easy_pnlt_map_code_example.R
+++ path_drc/easy_pnlt_map_code_example.R
@@ -16,21 +16,22 @@ library(grid)
 # --------------------
 
 # --------------------
+# set the user so caitlin can sometimes use your code
+
+user = 'caitlin'
+
 # set working directories
+dir = "C:/Users/ccarelli/Documents/pnls_data/"
+dt = readRDS(paste0(dir, "tb_hiv_ready_for_analysis.rds"))
+setwd('C:/Users/ccarelli/Documents/drc_shape_files/')
+  
+# subset the data table to only a single year
+# you can change this year
+year_set = 2018
+dt = dt[year(date)==year_set]
 
-  # local directory - read in the data from your directory
-  dt = readRDS("C:/Users/ckingongo/Documents/Atelier TB 2019/Data tb/tb_vih_analysis.rds")
-
-  # set working directory to the location of the shape file 
-  setwd("C:/Users/ckingongo/Documents/CARTOGRAPHIES/drc_shape_files/")
-
-  # subset the data table to only a single year
-  # you can change this year
-  year_set = 2018
-  dt = dt[year(date)==year_set]
-
-  # fix the total population variable
-  dt[ ,population_totale:=as.numeric(population_totale)]
+# fix the total population variable
+dt[ ,population_totale:=as.numeric(population_totale)]
 
 #---------------------------------------------------
 # import the shape file and format it to make maps
@@ -46,8 +47,7 @@ coord = data.table(coord)
 # sum the data to the DPS level
 # the data are currently at the facility level
 
-dt[dps=="kongo_central_ouest", dps:="kongo_central"]
-dt[dps=="kongo_central_est", dps:="kongo_central"]
+dt[grep("Kongo", dps), dps:="Kongo Central"]
 
 dps = dt[ ,.(population_totale=sum(population_totale, na.rm=T), 
              population_couverte=sum(population_couverte, na.rm=T),
@@ -70,41 +70,24 @@ map_names = coord[ ,unique(id)] # names of dps in the shape file
 data_names = dps[ ,unique(dps)] # names of dps in the data
 data_names[!(data_names %in% map_names)]
 
+
 # change the names in the shape file 
-coord[id=="Bas-U????l????", id:="bas_uele"]
-coord[id=="?????quateur" , id:="equateur"]
-coord[id=="Haut-Lomami" , id:="haut_lomami"]
-coord[id=="Haut-U????l????" , id:="haut_uele"]
-coord[id=="Kasa????", id:="kasai"]
-coord[id=="Kasa????-Central", id:="kasai_central"]
-coord[id=="Ma????-Ndombe", id:="mai_ndombe"]
-coord[id=="Nord-Kivu", id:="nord_kivu"]
-coord[id=="Nord-Ubangi", id:="nord_ubangi"]
-coord[id=="Sud-Kivu", id:="sud_kivu"]
-coord[id=="Sud-Ubangi", id:="sud_ubangi"]
-coord[id=="Kasa????-Oriental", id:="kasai_oriental"]
-coord[grep("ateur", id), id:="equateur"]
-coord[id=="Kongo-Central", id:="kongo_central"]
-coord[id=="Haut-Katanga", id:="haut_katanga"]
-coord[id=="Kasai Oriental", id:="kasai_oriental"]
-coord[id=="Bas-Uélé", id:="bas_uele"]
-coord[id=="Lomami", id:="lomami"]
-coord[id=="Maï-Ndombe", id:="mai_ndombe"]
-coord[id=="Kwango", id:="kwango"]
-coord[id=="Haut-Uélé", id:="haut_uele"]
-coord[id=="Lualaba", id:="lualaba"]
-coord[id=="Kasaï-Central", id:="kasai_central"]
-coord[id=="Kwilu", id:="kwilu"]
-coord[id=="Ituri", id:="ituri"]
-coord[id=="Maniema", id:="maniema"]
-coord[id=="Sankuru", id:="sankuru"]
-coord[id=="Tshopo", id:="tshopo"]
-coord[id=="Kasaï-Oriental", id:="kasai_oriental"]
-coord[id=="Kinshasa", id:="kinshasa"]
-coord[id=="Kasaï", id:="kasai"]
-coord[id=="Tanganyika", id:="tanganyika"]
-coord[id=="Tshuapa", id:="tshuapa"]
-coord[id=="Mongala", id:="mongala"]
+coord[id=="Bas-Uélé", id:="Bas Uele"]
+coord[id=="?????quateur" , id:="Equateur"]
+coord[id=="Haut-Lomami" , id:="Haut Lomami"]
+coord[id=="Haut-Uélé" , id:="Haut Uele"]
+coord[id=="Kasaï", id:="Kasai"]
+coord[id=="Kasaï-Central", id:="Kasai Central"]
+coord[id=="Maï-Ndombe", id:="Mai Ndombe"]
+coord[id=="Nord-Kivu", id:="Nord Kivu"]
+coord[id=="Nord-Ubangi", id:="Nord Ubangi"]
+coord[id=="Sud-Kivu", id:="Sud Kivu"]
+coord[id=="Sud-Ubangi", id:="Sud Ubangi"]
+coord[id=="Kasaï-Oriental", id:="Kasai Oriental"]
+coord[grep("ateur", id), id:="Equateur"]
+coord[id=="Kongo-Central", id:="Kongo Central"]
+coord[id=="Haut-Katanga", id:="Haut Katanga"]
+coord[id=="Kasai Oriental", id:="Kasai Oriental"]
 
 # check again
 map_names = coord[ ,unique(id)]
@@ -121,7 +104,7 @@ coord = merge(coord, dps, by='id', all=T)
 # graphs of the number of tb cases
 
 # total number of tb cases, 2018
-p1<- ggplot(coord, aes(x=long, y=lat, group=group, fill=total_de_cas)) + 
+ggplot(coord, aes(x=long, y=lat, group=group, fill=total_de_cas)) + 
   geom_polygon() + 
   scale_fill_gradientn('Total TB cases', colours=brewer.pal(9, 'Blues')) + 
   coord_fixed(ratio=1) + 
@@ -133,7 +116,7 @@ p1<- ggplot(coord, aes(x=long, y=lat, group=group, fill=total_de_cas)) +
   theme(plot.caption=element_text(size=14)) 
 
 # total number of tb cases, 2018, excluding kinshasa for scale
-p2<-ggplot(coord[id!='kinshasa'], aes(x=long, y=lat, group=group, fill=total_de_cas)) + 
+ggplot(coord[id!='kinshasa'], aes(x=long, y=lat, group=group, fill=total_de_cas)) + 
   geom_polygon() + 
   scale_fill_gradientn('Total TB cases, 2018, excluding Kinshasa for scale', 
                        colours=brewer.pal(9, 'Blues')) + 
@@ -149,7 +132,7 @@ p2<-ggplot(coord[id!='kinshasa'], aes(x=long, y=lat, group=group, fill=total_de_
 # now make a map of the total number of tb cases tested for hiv
 # use 'coord' and the variable 'tb_teste_vih'
 
-p3<-ggplot(coord, aes(x=long, y=lat, group=group, fill=tb_teste_vih)) + 
+ggplot(coord, aes(x=long, y=lat, group=group, fill=tb_teste_vih)) + 
   geom_polygon() + 
   scale_fill_gradientn('total number of tb cases tested for hiv', colours=brewer.pal(9, 'Reds')) + 
   coord_fixed(ratio=1) + 
@@ -162,7 +145,7 @@ p3<-ggplot(coord, aes(x=long, y=lat, group=group, fill=tb_teste_vih)) +
 
 #---------------------------------------------------
 #tb_vih_positif
-p4<-ggplot(coord[], aes(x=long, y=lat, group=group, fill=tb_vih_positif)) + 
+-ggplot(coord[], aes(x=long, y=lat, group=group, fill=tb_vih_positif)) + 
   geom_polygon() + 
   scale_fill_gradientn('total number of tb cases tested for hiv positif', colours=brewer.pal(9, 'Reds')) + 
   coord_fixed(ratio=1) + 
@@ -173,17 +156,51 @@ p4<-ggplot(coord[], aes(x=long, y=lat, group=group, fill=tb_vih_positif)) +
   theme_minimal(base_size=20) + 
   theme(plot.caption=element_text(size=14))
 #---------------------------------------------------
-#
 # Map percentage of tb cases tested for hiv
-p5<-ggplot(coord, aes(x=long, y=lat, group=group, fill=tx)) + 
+
+
+ggplot(coord, aes(x=long, y=lat, group=group, fill=tx)) + 
   geom_polygon() + 
-  scale_fill_gradientn('percentage of tb cases tested for hiv', colours=brewer.pal(9, 'Spectral')) + 
+  scale_fill_gradientn('Percent (%)', colours=brewer.pal(9, 'Spectral')) + 
   coord_fixed(ratio=1) + 
   scale_x_continuous('', breaks = NULL) + 
   scale_y_continuous('', breaks = NULL) + 
-  labs(title='percentage tb tested hiv(%), 2018', 
-       caption='Source: PNLT data') + 
+  labs(title='Percentage TB cases tested for HIV, 2018', 
+       caption='Source: PNLT program data') + 
   theme_minimal(base_size=20) + 
   theme(plot.caption=element_text(size=14))
+
+# --------------------------------------------------------------------
+# create labels for the regional maps
+
+# identify centroids and label them
+names = data.table(coordinates(map))
+setnames(names, c('long', 'lat'))
+names[ , id:=unique(coord$id)]
+
+dps[ , label:=paste0(id, ": ", tx, "%")]
+labels = dps[ ,.(id, label)]
+names = merge(names, labels, by='id', all=T)
+
+pdf(paste0(dir, 'tb_tested_map.pdf'), width=12, height=9)
+
+ggplot(coord, aes(x=long, y=lat, group=group, fill=tx)) + 
+  geom_polygon() + 
+  scale_fill_gradientn('Percent (%)', colours=brewer.pal(9, 'RdYlBu')) + 
+  coord_fixed(ratio=1) + 
+  scale_x_continuous('', breaks = NULL) + 
+  scale_y_continuous('', breaks = NULL) + 
+  labs(title='Percentage TB cases tested for HIV, 2018', 
+       caption='Source: PNLT program data') + 
+  theme_minimal(base_size=20) + 
+  theme(text = element_text(size=18)) +
+geom_label_repel(data = names, aes(label = label, x = long, y = lat, group = id), inherit.aes=FALSE, size=5)
+
+
+dev.off()
+
+
+
+
 
 
